529502fd1a86cf6ed6f6dc083222f96ddf04bf24
Nishant-66 <kumarnishant26346@gmail.com>
2024-07-10 20:09:23 +0530

    Connecting databases and Creating homepage of the chat app

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express=require('express');
+const dotenv=require('dotenv');
+const connectDB = require('./config/db');
+
 const app=express();
+
+dotenv.config();
+connectDB();
 app.get('/', function(req, res) {
     res.send("Hello");
 });
